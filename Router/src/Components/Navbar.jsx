53180520eff1f63f0323149b937a055d06d3798f
Max3796 <gurpreet.singh@kalvium.community>
2024-03-08 17:40:26 +0530

    Done

--- Router/src/Components/Navbar.jsx
+++ Router/src/Components/Navbar.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div style={{padding:"30px",display:"flex",justifyContent:"space-between",alignItems:"center",backgroundColor:"white",color:"black"}}>
+    <div
+      style={{
+        padding: "30px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "white",
+        color: "black",
+      }}
+    >
+      <Link style={{ fontSize: "22px", color: "black" }} to={"/"}>
         <h1>
-            KALVIUM
-            <img src="https://kalvium.community/images/sidebar-3d-logo.svg" alt="" />
+          KALVIUM
+          <img
+            src="https://kalvium.community/images/sidebar-3d-logo.svg"
+            alt=""
+          />
         </h1>
-        <div style={{display: "flex",marginLeft:"30px"}}>
-            <Link style={{fontSize:"30px",color:"black"}} to={"/about"}>About</Link>
-            <Link style={{marginLeft:"30px",fontSize:"30px",color:"black"}} to={"/contact"}>Contact</Link>
-        </div>
+      </Link>
+
+      <div style={{ display: "flex", marginLeft: "30px" }}>
+        <Link style={{ fontSize: "30px", color: "black" }} to={"/about"}>
+          About
+        </Link>
+        <Link
+          style={{ marginLeft: "30px", fontSize: "30px", color: "black" }}
+          to={"/contact"}
+        >
+          Contact
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
